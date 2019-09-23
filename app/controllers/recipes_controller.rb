426306abe76b373250fa8f94685bc9182243988c
Uchenna A. Iheanacho <uchenna.a.i@gmail.com>
2019-09-23 02:42:07 +0100

    Add recipes routes

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,53 @@
+class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :update, :destroy]
+  before_action :authenticate_user!, only: [:create, :update, :destroy]
+
+  # GET /recipes
+  def index
+    @recipes = Recipe.all
+
+    render json: @recipes
+  end
+
+  # GET /recipes/1
+  def show
+    render json: @recipe
+  end
+
+  # POST /recipes
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user = current_user
+
+    if @recipe.save
+      render json: @recipe, status: :created, location: @recipe
+    else
+      render json: @recipe.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /recipes/1
+  def update
+    if @recipe.user == current_user && @recipe.update(recipe_params)
+      render json: @recipe
+    else
+      render json: @recipe.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /recipes/1
+  def destroy
+    @recipe.destroy if @recipe.user == current_user
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_recipe
+      @recipe = Recipe.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def recipe_params
+      params.require(:recipe).permit(:title, :description, :user_id)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  scope :api, respond_to: { format: :json } do
+  scope :api, defaults: { format: :json } do
     mount_devise_token_auth_for 'User', at: 'auth'
+    resources :recipes
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
