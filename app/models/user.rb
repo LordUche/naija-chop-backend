581464f748fa914540e30603e61c964e359bae37
Uchenna A. Iheanacho <uchenna.a.i@gmail.com>
2019-09-23 03:21:11 +0100

    Define following relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,24 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
   has_many :recipes
+  has_many :active_relationships, class_name: 'Relationship',
+                                  foreign_key: 'follower_id',
+                                  dependent: :destroy
+  has_many :passive_relationships, class_name: 'Relationship',
+                                   foreign_key: 'followed_id',
+                                   dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    following.delete other_user
+  end
+
+  def following?(other_user)
+    following.include? other_user
+  end
 end
